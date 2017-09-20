c2ade885f200c5d4dd8b6ed64c286ab701ea9bfc
Laure Daumal <daumal_l@epita.fr>
2017-09-20 19:28:35 +0200

    add classification things + results of processing

--- src/learning_direction/learn.py
+++ src/learning_direction/learn.py
@@ -9,27 +9,33 @@ import scoring
 from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 CLF_FOLDER = "tmp/"
-CLF_NAME = "onevsrest"
-DEBUG = True
+CLF_NAME = "forest_defaultparams"
+DEBUG = False
 
 dataset = np.load("dataset.npy")
 labels = np.load("labels.npy")
+print("***")
+print("Result for classifier: %s" % CLF_NAME)
 if DEBUG:
     print("Dataset loaded.")
 X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.3, random_state=42)
 
+print("Training samples: %d\tTesting samples: %d" % (len(X_train), len(X_test)))
 # Different classifiers
 '''
 clf = svm.SVC(kernel='linear', C = 1.0)
 clf = GaussianNB()
-'''
 clf = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True))
-if DEBUG:
-    print("Learning phase done.")
+'''
+clf = RandomForestClassifier()
+
 
 clf = clf.fit(X_train, y_train)
+if DEBUG:
+    print("Learning phase done.")
 
 y_pred = clf.predict(X_test)
 scoring.get_confusion_matrix(y_test, y_pred)
