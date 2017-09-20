1e284444f38d25d3a8a8fcec39e501f59618fe8c
Laure Daumal <daumal_l@epita.fr>
2017-09-20 18:02:25 +0200

    update learning code

--- src/learning_direction/learn.py
+++ src/learning_direction/learn.py
@@ -8,23 +8,28 @@ import scoring
 # classifiers
 from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
+from sklearn.multiclass import OneVsRestClassifier
 
 CLF_FOLDER = "tmp/"
-CLF_NAME = "gaussian"
+CLF_NAME = "onevsrest"
+DEBUG = True
 
 dataset = np.load("dataset.npy")
 labels = np.load("labels.npy")
-
+if DEBUG:
+    print("Dataset loaded.")
 X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.3, random_state=42)
 
-#clf = svm.SVC(kernel='linear', C = 1.0)
+# Different classifiers
+'''
+clf = svm.SVC(kernel='linear', C = 1.0)
 clf = GaussianNB()
+'''
+clf = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True))
+if DEBUG:
+    print("Learning phase done.")
 
 clf = clf.fit(X_train, y_train)
-
-#f = open("clf")
-#s = pickle.dump(clf, f)
-#f.close()
 
 y_pred = clf.predict(X_test)
 scoring.show_confusion_matrix(y_test, y_pred)
