512781f67c14cb2db3724055cbf0e2427a851f60
Laure Daumal <daumal_l@epita.fr>
2017-09-20 17:59:10 +0200

    update image processing: add resizing of images, change threshold from 200 to 160

--- src/image-processing/build_dataset.py
+++ src/image-processing/build_dataset.py
@@ -1,22 +1,37 @@
-import parse
 import numpy as np
-import filters
+import scipy
 from sklearn import svm
+
+import filters
+import parse
 
 FOLDER = "client/pictures/"
 DATASET_NAME = "D2-test1/"
-DEBUG = True
+SIZE = 80*60
+DEBUG = False
 
 files = parse.get_filenames(FOLDER + DATASET_NAME)
-size = filters.binarize(files[0], 200).size
-dataset = np.ones((len(files), size))
+dataset = np.ones((len(files), SIZE))
 labels = np.ones(len(files))
+
+class Counter:
+    i = 0
+
+# Pipeline
+def process_image(filename):
+    img = filters.binarize(filename, 160)
+    img = scipy.misc.imresize(img, (80, 60), interp="nearest")
+    if DEBUG:
+        scipy.misc.imsave(FOLDER + "tmp/" + str(Counter.i).zfill(3) + ".jpg", img)
+        Counter.i += 1
+    img = img.reshape(SIZE)
+    return img
+
 
 for i, f in enumerate(files):
     if DEBUG:
         print("Handling file %s" % f)
-    binary = filters.binarize(f, 200)
-    dataset[i] = binary.reshape(size)
+    dataset[i] = process_image(f)
     labels[i] = parse.labelize(f)
 
 np.save("dataset", dataset)
--- src/image-processing/filters.py
+++ src/image-processing/filters.py
@@ -2,7 +2,7 @@ from PIL import Image
 from scipy.misc import imsave
 import numpy as np
 
-def bin_array(numpy_array, threshold=200):
+def bin_array(numpy_array, threshold):
     """Binarize a numpy array."""
     for i in range(len(numpy_array)):
         for j in range(len(numpy_array[0])):
@@ -12,7 +12,7 @@ def bin_array(numpy_array, threshold=200):
                 numpy_array[i][j] = 0
     return numpy_array
 
-def binarize(filename, threshold):
+def binarize(filename, threshold=170):
     """Binarize an image """
     file = Image.open(filename)
     file = file.convert('L') # 'L' is monochrome, '1' is black and white
