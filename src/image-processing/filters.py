0c8807dcef72088aa8571b780b6d3d21a2f1393d
Laure Daumal <daumal_l@epita.fr>
2017-10-16 15:03:35 +0200

    old modifications

--- src/image-processing/filters.py
+++ src/image-processing/filters.py
@@ -12,10 +12,13 @@ def bin_array(numpy_array, threshold):
                 numpy_array[i][j] = 0
     return numpy_array
 
-def binarize(filename, threshold=170):
-    """Binarize an image """
-    file = Image.open(filename)
+def bin_image(img, threshold=170):
     file = file.convert('L') # 'L' is monochrome, '1' is black and white
     file = np.array(file)
     file = bin_array(file, threshold)
     return file
+
+def binarize(filename, threshold=170):
+    """Binarize an image """
+    file = Image.open(filename)
+    return bin_image(file, threshold)
