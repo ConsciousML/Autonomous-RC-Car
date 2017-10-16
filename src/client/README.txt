cbfdab1939a8e7b243e7f14559521f41d9608759
Axel Mendoza <axel.mendoza@epita.fr>
2017-10-16 18:40:48 +0200

    implementing xbox controller

--- src/client/README.txt
+++ src/client/README.txt
@@ -1,5 +1,11 @@
 Introduction:
 	This is Sunfounder Raspberry Pi smart video car client. It is based on Python.
 
+Xbox Controller:
+    sudo apt-get install xboxdrv
+    sudo apt-get dist-upgrade
+    sudo xboxdrv
+    sudo xboxdrv --detach-kernel-driver
+
 Notice:
 	Before you run the client routine, you must first run the server routine.
---
+++ src/client/client_controller.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from socket import *      # Import necessary modules
+import numpy
+import cv2
+import os
+import pickle
+import scipy
+import filters
+import xbox
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+import PIL
+
+
+os.system('xset r off')
+ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
+
+
+HOST = '192.168.43.46'    # Laure (Ionis's Down) IP address
+HOST = '172.20.10.11'     # Thibaut (iPhone) IP address
+PORT = 21567
+BUFSIZ = 1024             # buffer size
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)   # Create a socket
+tcpCliSock.connect(ADDR)                    # Connect with the server
+tcpCliSock.setblocking(1)
+
+dira = 0
+
+dic_dir = {-1: "left", 0: "forward", 1: "right"}
+dic_dir_l = {1: "left", 0: "home", 2: "right"}
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+class Counter:
+    i = 1437
+
+def get_img():
+    length = recvall(tcpCliSock, 16)
+    print 'rec len'
+    stringData = recvall(tcpCliSock, int(length))
+    print 'rec img'
+    img = numpy.fromstring(stringData, dtype='uint8')
+    imgdec = cv2.imdecode(img, 1)
+    return imgdec
+
+'''
+def get_image():
+    img = tcpCliSock.recv()
+
+top.bin("<<GetImg>>", get_image)
+top.event_generate("<<GetImg>>", when="tail")
+'''
+
+def process_dir(dir):
+    print dir
+    tcpCliSock.send(dir)
+    get_img(dir)
+
+# =============================================================================
+# The function is to send the command forward to the server, so as to make the
+# car move forward.
+# =============================================================================
+def forward_fun(event):
+    process_dir("forward")
+
+def backward_fun(event):
+    process_dir("backward")
+
+def left_fun(event):
+    global dira
+    dira = -1
+    process_dir("left")
+
+def right_fun(event):
+    global dira
+    dira = 1
+    process_dir("right")
+
+def stop_fun(event):
+    process_dir('stop')
+
+def home_fun(event):
+    global dira
+    dira = 0
+    process_dir('home')
+
+def x_increase(event):
+    process_dir('x+')
+
+def x_decrease(event):
+    process_dir('x-')
+
+def y_increase(event):
+    process_dir('y+')
+
+def y_decrease(event):
+    process_dir('y-')
+
+def xy_home(event):
+    process_dir('xy_home')
+
+spd = 35
+
+def changeSpeed():
+    tmp = 'speed'
+    global spd
+    data = tmp + str(spd)  # Change the integers into strings and combine them with the string 'speed'.
+    print 'sendData = %s' % data
+    tcpCliSock.send(data)  # Send the speed data to the server(Raspberry Pi)
+
+
+def main():
+    #get_img()
+    changeSpeed()
+
+    rep = tcpCliSock.recv(64)
+    #get_img()
+
+    tcpCliSock.recv(64)
+    joy = xbox.Joystick()
+    last_trig = False
+
+    while True:
+        t = joy.rightTrigger()
+        cur_trig = t > 0
+        if (last_trig == cur_trig):
+            continue
+        last_trig = cur_trig
+        if (cur_trig == 0.0):
+            data = 'stop'
+        if (cur_trig > 0):
+            data = 'forward'
+        print(data)
+        tcpCliSock.send(data)
+        tcpCliSock.recv(64)
+
+        """tcpCliSock.send("OK")
+        get_img()
+        tcpCliSock.send("stop")
+        tcpCliSock.recv(64)
+
+        tcpCliSock.send("OK")
+        get_img()
+        tcpCliSock.send("forward")
+        tcpCliSock.recv(64)"""
+
+
+if __name__ == '__main__':
+	main()
