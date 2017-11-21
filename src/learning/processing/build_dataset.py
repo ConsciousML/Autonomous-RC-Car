2f7032bac2947f169f278099d2fc80c8c494da3b
Laure Daumal <daumal_l@epita.fr>
2017-11-21 17:28:13 +0100

    update dataset building: egalize data class

--- src/learning/processing/build_dataset.py
+++ src/learning/processing/build_dataset.py
@@ -1,12 +1,15 @@
 import numpy as np
 from scipy.misc import imread, imresize
 from sklearn import svm
+from random import shuffle
 
-from processing import filters
-from processing import parse
+import filters
+import parse
 
 NPY_DATA_NAME = "dataset"
 NPY_LABELS_NAME = "labels"
+
+def min(a, b) : return a if a < b else b
 
 def dump_npy():
     FOLDER = "client/dataset01/"
@@ -16,12 +19,28 @@ def dump_npy():
     dataset = []
     labels = []
 
-    files += parse.get_filenames(FOLDER + "forward")
-    files += parse.get_filenames(FOLDER + "left")
-    files += parse.get_filenames(FOLDER + "right")
+    files_l1 = parse.get_filenames(FOLDER + "forward")
+    min_len = len(files_l1)
+
+    files_l2 = parse.get_filenames(FOLDER + "left")
+    min_len = min(min_len, len(files_l2))
+
+    files_l3 = parse.get_filenames(FOLDER + "right")
+    min_len = min(min_len, len(files_l3))
+
+    print(min_len)
+
+    shuffle(files_l1)
+    shuffle(files_l2)
+    shuffle(files_l3)
+
+    files += files_l1[:min_len]
+    files += files_l2[:min_len]
+    files += files_l3[:min_len]
+
+    print(len(files))
 
     for i, f in enumerate(files):
-        print("Handling file %s" % f)
         dataset.append(f)
         labels.append(parse.labelize_dir(f))
 
@@ -39,6 +58,7 @@ def gen_data(f_dataset, f_labels, batch_size):
     f_labels : nom du fichier .npy où se trouvent les labels
     batch_size : le nombre de fichiers à envoyer par batch
     '''
+    dump_npy()
     dataset = np.load(f_dataset)
     labels = np.load(f_labels)
     j = 0
@@ -58,3 +78,4 @@ def gen_data(f_dataset, f_labels, batch_size):
         grappe_data.append(readNshape(dataset[i]))
         grappe_labels.append(labels[i])
         j += 1
+
