c8ecb8bb9e026abc063a7987242acb05015788de
Axel Mendoza <axel.mendoza@epita.fr>
2020-10-06 16:45:43 -0500

    add utils module with files.py

---
+++ smartcar/utils/files.py
@@ -0,0 +1,23 @@
+import os
+
+"""
+
+This module contains function the gather, move or modify files.
+
+"""
+
+def get_data_paths(dir):
+    """Returns the images and labels from a directory"""
+    image_plist = []
+    label_plist = []
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            if not f.endswith('.jpg'):
+                continue
+            image_path = os.path.join(root, f)
+            label_path = image_path[:-4] + '.json'
+            if not os.path.exists(label_path):
+                continue
+            image_plist.append(image_path)
+            label_plist.append(label_path)
+    return image_plist, label_plist
