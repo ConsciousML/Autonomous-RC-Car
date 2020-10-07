77288b0a5fe21d2c99f0a55470d3b7e6f1bebfb5
Axel Mendoza <axel.mendoza@epita.fr>
2020-10-07 16:35:18 -0500

    add datetime str to save outputs

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from datetime import datetime
 from sklearn.model_selection import train_test_split
 
 sys.path.append('..')
@@ -19,8 +20,14 @@ if __name__ == "__main__":
     data_dir = "C:\Projects\SmartCar\data"
     lr = 1e-4
     batch_size = 16
-    model_save_path = 'test.h5' 
+    output_dir = '' 
     epochs = 20
+
+    now = datetime.now()
+    dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
     image_fnames, label_fnames = get_data_paths(data_dir)
     X_train, X_test, y_train, y_test = train_test_split(image_fnames, label_fnames, train_size=0.80, random_state=42)
@@ -42,4 +49,10 @@ if __name__ == "__main__":
                            shuffle=True,
                            callbacks=[checkpointer])
     print('Best model saved to: ' + model_save_path)
+
+    plt.plot(hist.history["loss"][1:])
+    plt.plot(hist.history["val_loss"][1:])
+    ax = plt.gca()
+    ax.set_ylim([0.06, 0.11])
+    plt.savefig('training_curve.jpg')
      
