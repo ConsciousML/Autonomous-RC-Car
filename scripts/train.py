c0e5d75a265c87576f0ad61a56346ba57b121582
Axel Mendoza <axel.mendoza@epita.fr>
2020-10-07 16:58:54 -0500

    save training curves to out_dir

--- scripts/train.py
+++ scripts/train.py
@@ -1,9 +1,13 @@
 import os
 import sys
+import argparse
+import matplotlib.pyplot as plt
 from datetime import datetime
 from sklearn.model_selection import train_test_split
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 
-sys.path.append('..')
+sys.path.append(os.getcwd())
 from smartcar.utils.path import get_data_paths
 from smartcar.learn.model import CustomCNN
 from smartcar.learn.generator import CustomGenerator
@@ -20,8 +24,8 @@ if __name__ == "__main__":
     data_dir = "C:\Projects\SmartCar\data"
     lr = 1e-4
     batch_size = 16
-    output_dir = '' 
-    epochs = 20
+    output_dir = 'tmp' 
+    epochs = 3
 
     now = datetime.now()
     dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
@@ -41,8 +45,8 @@ if __name__ == "__main__":
     model.compile(optimizer=opt,
               loss={'out' : 'mean_squared_error'})    
 
-    model_path = os.path.abspath(model_save_path)
-    checkpointer = ModelCheckpoint(model_path, save_best_only=True, monitor='val_loss', mode='min')
+    model_save_path = os.path.join(output_dir, dt_string + '.h5')
+    checkpointer = ModelCheckpoint(model_save_path, save_best_only=True, monitor='val_loss', mode='min')
     hist = model.fit_generator(generator=datagen_train,
                            validation_data=datagen_test,
                            epochs=epochs,
@@ -50,9 +54,12 @@ if __name__ == "__main__":
                            callbacks=[checkpointer])
     print('Best model saved to: ' + model_save_path)
 
+    loss_fig_path = os.path.join(output_dir, dt_string + '.jpg')
     plt.plot(hist.history["loss"][1:])
     plt.plot(hist.history["val_loss"][1:])
     ax = plt.gca()
     ax.set_ylim([0.06, 0.11])
-    plt.savefig('training_curve.jpg')
+    plt.savefig(loss_fig_path)
+
+    print('Save loss figure to: ' + loss_fig_path)
      
